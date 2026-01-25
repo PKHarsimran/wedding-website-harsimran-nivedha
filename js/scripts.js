a9786abb1e516304030f03da22c7537f2f29e36f
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2026-01-25 07:14:53 -0400

    Update invite code post URL in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -222,7 +222,7 @@ $(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxpehyyHgXRhODAyU6ECgBsh6rIn4kg9CVQnDiKetDPyjeVFpPF21vBz45t9cr4rOE1Nw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
